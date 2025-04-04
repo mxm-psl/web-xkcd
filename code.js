b8311dbf61dc66da7c6897b27bb44bb31f2f3837
mxm-psl <maximepivert14@gmail.com>
2025-04-04 17:16:23 +0200

    xkcd

--- code.js
+++ code.js
@@ -1,3 +1,97 @@
 const XKCD = "https://xkcd.now.sh/?comic="
 
-// write your code here
+/// ce que je rajoute :
+
+let currentComic = null;
+let latestComic = null;
+
+const fetchIssue = (num) => {
+    return fetch(XKCD + num).then((response) => {
+        if (response.ok) {
+            console.log(response);
+            return response.json();
+        }
+        else {
+            console.error("Problem");
+        }
+    }).then((data) => {
+        console.log("dans le second then");
+        console.log(data);
+        
+        console.log(`Image num: ${data.num}`);
+
+
+        currentComic = data.num;
+
+
+        if (!latestComic) {
+        latestComic = data.num;
+        }
+
+
+        updateButtons();
+
+
+
+        if (!data) return;
+
+
+        document.getElementById("num").textContent = data.num;
+
+
+        const imgElement = document.querySelector("#xkcd img");
+        imgElement.src = data.img;
+        imgElement.alt = data.title;
+
+    })
+  };
+
+
+  const next = () => {
+    if (currentComic < latestComic) {
+        fetchIssue(currentComic + 1);
+    }
+};
+
+const previous = () => {
+    if (currentComic > 1) {
+        fetchIssue(currentComic - 1);
+    }
+};
+
+
+
+
+const updateButtons = () => {
+    document.getElementById("previous").disabled = (currentComic <= 1);
+    document.getElementById("next").disabled = (currentComic >= latestComic);
+};
+
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const resetButton = document.getElementById("reset");
+
+    document.getElementById("previous").addEventListener("click", previous);
+    document.getElementById("next").addEventListener("click", next);
+    document.getElementById("reset").addEventListener("click", () => fetchIssue("latest"));
+
+    
+    
+    const fetchAndDisplayIssue = (num) => {
+        fetchIssue(num).then((data) => {
+            if (data) {
+                document.getElementById("num").textContent = data.num;
+            }
+        });
+    };
+
+    
+    resetButton.addEventListener("click", () => fetchAndDisplayIssue("latest"));
+
+    
+    fetchAndDisplayIssue("latest");
+});
+
+
+
